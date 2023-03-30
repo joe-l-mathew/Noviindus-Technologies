b94b2a090cc861c03d9f57f421513f8f8acb2eea
joe-l-mathew <84284015+joe-l-mathew@users.noreply.github.com>
2023-03-30 15:52:26 +0530

    Migrated to HTTP package

--- lib/providers/login_provider.dart
+++ lib/providers/login_provider.dart
@@ -1,25 +1,27 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
 import '../models/user_login_model.dart';
 import '../utilities/user_provider.dart';
 import '../widgets/show_snackbar.dart';
-import 'api_constants.dart';
 
 class LoginProvider {
-  final dio = Dio();
-
   Future<bool> loginUser(
       String userName, String password, BuildContext context) async {
-    Response response = await dio.post('${ApiConstants.baseUrl}/LoginApi',
-        data: {'username': userName, 'password': password});
-    if (response.data['status'] == true) {
-      UserLoginModel user = UserLoginModel.fromJson(response.data);
+    String url = "http://flutter.noviindus.co.in/api/LoginApi";
+    final response = await http.post(Uri.parse(url),
+        body: {'username': 'admin_user', 'password': '123admin789'});
+    var responseData = json.decode(response.body);
+    if (responseData['status'] == true) {
+      UserLoginModel user = UserLoginModel.fromJson(responseData);
       // ignore: use_build_context_synchronously
       Provider.of<UserProvider>(context, listen: false).setUser(user);
       return true;
     } else {
+      // ignore: use_build_context_synchronously
       showSnackbar(context, "Invalid Credentials");
       return false;
     }
